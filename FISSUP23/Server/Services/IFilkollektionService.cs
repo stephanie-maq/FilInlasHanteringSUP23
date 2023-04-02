8a611c7afc2dd489616822e421eee896c1ba3af5
Stephanie <stephanie.maqdis@icloud.com>
2023-04-03 00:36:44 +0200

    wip

---
+++ FISSUP23/Server/Services/IFilkollektionService.cs
@@ -0,0 +1,14 @@
+﻿using FISSUP23.Server.ApiModels;
+
+namespace FISSUP23.Server.Services
+{
+    public interface IFilkollektionService
+    {
+        Task<List<ApiFilkollektion>> Get();
+        Task<List<ApiFilkollektion>> GetByOverforingId(int id);
+        Task<ApiFilkollektion> GetById(int id);
+        Task Add(ApiFilkollektion apiFilkollektion);
+        Task Update(int id);
+        Task Delete(int id);
+    }
+}
