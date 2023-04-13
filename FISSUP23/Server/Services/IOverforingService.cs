5fb04b8f80fe3fa16743f493db6ec5e82810c27d
Stephanie Maqdis Anton <84038847+stephanie-maq@users.noreply.github.com>
2023-04-13 16:37:45 +0200

    Show new överföringar(Not in nyÖverföringar tabell) (#6)

--- FISSUP23/Server/Services/IOverforingService.cs
+++ FISSUP23/Server/Services/IOverforingService.cs
@@ -7,7 +7,8 @@ namespace FISSUP23.Server.Services.Interface
     {
         Task<List<Overforing>> GetOverforingar();
         Task<Overforing> GetByID(int id);
-        Task Add(NyOverforing overforing);
+        Task Add(Overforing overforing);
+
         Task Update(int id, Overforing overforing);
 
         Task Delete(List<string> ids);
