2ca16ec730aafb03881f3a532967848bbb3550a8
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2023-04-25 22:36:34 +0200

    Add one direction relationship (#25)

--- FISSUP23.Database/Models/SsisGenericReadContext.cs
+++ FISSUP23.Database/Models/SsisGenericReadContext.cs
@@ -143,6 +143,16 @@ public partial class SsisGenericReadContext : DbContext
             entity.Property(e => e.Namn)
                 .HasMaxLength(255)
                 .IsUnicode(false);
+            entity.HasOne<Overforing>()
+                .WithMany(p => p.FilKollektions)
+                .HasForeignKey(d => d.FilTypId)
+                .HasConstraintName("FK_FilKollektion_Filtyp")
+                .OnDelete(DeleteBehavior.ClientCascade);
+            entity.HasOne<Overforing>()
+                .WithMany(p => p.FilKollektions)
+                .HasForeignKey(d => d.OverforingId)
+                .HasConstraintName("FK_FilKollektion_Overforing")
+                .OnDelete(DeleteBehavior.ClientCascade);
         });
 
         modelBuilder.Entity<Filtyp>(entity =>
